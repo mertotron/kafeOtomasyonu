86c01a08af71401cfd9ef3d85727f7f235fc743a
Mert Ali CİRHİNLİOĞLU <mert_ali_58cir@hotmail.com>
2025-05-02 11:07:47 +0300

    muhasebe paneli başlandı veri tabanı yedek alma aylık günlük ve haftalık olarak tabloların getirilmesi işlemleri tamamlandı

--- kafeOtomasyonu/Views/MuhasebePaneli.Designer.cs
+++ kafeOtomasyonu/Views/MuhasebePaneli.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MuhasebePaneli));
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
@@ -48,6 +49,7 @@
             this.button1.TabIndex = 0;
             this.button1.Text = "BİR AYLIK RAPOR GETİR";
             this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // button2
             // 
@@ -59,6 +61,7 @@
             this.button2.TabIndex = 1;
             this.button2.Text = "HAFTALIK RAPOR GETİR";
             this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // button3
             // 
@@ -70,6 +73,7 @@
             this.button3.TabIndex = 2;
             this.button3.Text = "GÜNLÜK RAPOR GETİR";
             this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
             // button4
             // 
@@ -81,6 +85,7 @@
             this.button4.TabIndex = 3;
             this.button4.Text = "VERİ YEDEĞİ AL";
             this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
             // 
             // dataGridView1
             // 
@@ -110,6 +115,7 @@
             this.button5.TabIndex = 6;
             this.button5.Text = "RAPORU DIŞA AKTAR (EXEL)";
             this.button5.UseVisualStyleBackColor = false;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
             // 
             // MuhasebePaneli
             // 
@@ -124,7 +130,10 @@
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "MuhasebePaneli";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "MuhasebePaneli";
             this.Load += new System.EventHandler(this.MuhasebePaneli_Load);
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
--- kafeOtomasyonu/Views/MuhasebePaneli.cs
+++ kafeOtomasyonu/Views/MuhasebePaneli.cs
@@ -8,6 +8,8 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using Excel = Microsoft.Office.Interop.Excel;
+using System.Data.SqlClient;
+using System.IO;
 
 namespace kafeOtomasyonu.Views
 {
@@ -18,48 +20,84 @@ namespace kafeOtomasyonu.Views
         {
             InitializeComponent();
         }
+        public void VeritabaniYedekAl(SqlConnection conn)
+        {
+            try
+            {
+                if (conn.State != ConnectionState.Open)
+                    conn.Open();
+
+                string databaseName = conn.Database;
+
+                // Tarih formatı: yyyyMMdd_HHmmss
+                string tarih = DateTime.Now.ToString("yyyyMMdd_HHmmss");
+
+                // Yedekleme dizini (projeyle aynı dizindeki sabit klasör)
+                string yedekKlasoru = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, @"DatabaseBackup\");
+
+                // Eğer klasör yoksa oluştur
+                if (!Directory.Exists(yedekKlasoru))
+                    Directory.CreateDirectory(yedekKlasoru);
+
+                // Tam yedek dosya yolu
+                string yedekDosyaYolu = Path.Combine(yedekKlasoru, $"veritabani_yedek_{tarih}.bak");
+
+                // SQL backup komutu
+                string backupQuery = $@"BACKUP DATABASE [{databaseName}] TO DISK = @yedekYolu WITH INIT, FORMAT";
+
+                using (SqlCommand cmd = new SqlCommand(backupQuery, conn))
+                {
+                    cmd.Parameters.AddWithValue("@yedekYolu", yedekDosyaYolu);
+                    cmd.ExecuteNonQuery();
+                }
+
+                MessageBox.Show($"Veritabanı yedeği alındı:\n{yedekDosyaYolu}", "Başarılı", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Yedek alma hatası: " + ex.Message, "Hata", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            finally
+            {
+                if (conn.State == ConnectionState.Open)
+                    conn.Close();
+            }
+        }
 
 
         private void MuhasebePaneli_Load(object sender, EventArgs e)
         {
-            // VERİLERİ DATA GRİD VİEWE ÇEKEN SORGU
-            //SELECT
-            //u.UrunAdi,
-            //sd.Adet,
-            //sd.Fiyat,
-            //(sd.Adet * sd.Fiyat) AS ToplamFiyat,
-            //s.Tarih
-            //FROM SiparisDetay sd
-            //INNER JOIN Siparisler s ON sd.SiparisId = s.Id
-            //INNER JOIN Urunler u ON sd.UrunId = u.UrunID
-            //WHERE s.Tarih >= @BaslangicTarihi AND s.Tarih <= @BitisTarihi
-
-
-
-            //buton kodları 
-            //private void btnGunluk_Click(object sender, EventArgs e)
-            //{
-            //    DateTime bugun = DateTime.Today;
-            //    RaporuGetir(bugun, bugun.AddDays(1).AddTicks(-1)); // 00:00 - 23:59
-            //}
-
-            //private void btnHaftalik_Click(object sender, EventArgs e)
-            //{
-            //    DateTime baslangic = DateTime.Today.AddDays(-7);
-            //    DateTime bitis = DateTime.Now;
-            //    RaporuGetir(baslangic, bitis);
-            //}
-
-            //private void btnAylik_Click(object sender, EventArgs e)
-            //{
-            //    DateTime baslangic = DateTime.Today.AddDays(-30);
-            //    DateTime bitis = DateTime.Now;
-            //    RaporuGetir(baslangic, bitis);
-            //}
-
 
         }
         //exele data grid view 1 deki veirleri aktar 
+        public void raporGetir(DateTime baslangic, DateTime bitis)
+        {
+            using (SqlConnection conn = Database.Db.GetConnection())
+            {
+                string query = @"SELECT 
+                            u.UrunAdi,
+                            sd.Adet,
+                            sd.Fiyat,
+                            (sd.Adet * sd.Fiyat) AS ToplamFiyat,
+                            s.Tarih
+                         FROM SiparisDetay sd
+                         INNER JOIN Siparisler s ON sd.SiparisId = s.Id
+                         INNER JOIN Urunler u ON sd.UrunId = u.UrunID
+                         WHERE s.Tarih BETWEEN @baslangic AND @bitis";
+
+                using (SqlCommand cmd = new SqlCommand(query, conn))
+                {
+                    cmd.Parameters.AddWithValue("@baslangic", baslangic);
+                    cmd.Parameters.AddWithValue("@bitis", bitis);
+
+                    SqlDataAdapter da = new SqlDataAdapter(cmd);
+                    DataTable dt = new DataTable();
+                    da.Fill(dt);
+
+                    dataGridView1.DataSource = dt;
+                }
+            }
+        }
         private void ExportToExcel(DataGridView dataGridView)
         {
             // Excel uygulamasını başlat
@@ -112,6 +150,36 @@ namespace kafeOtomasyonu.Views
                 System.Runtime.InteropServices.Marshal.ReleaseComObject(workbook);
                 System.Runtime.InteropServices.Marshal.ReleaseComObject(excelApp);
             }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            DateTime bugun = DateTime.Today;
+            raporGetir(bugun, bugun.AddDays(1).AddTicks(-1)); // 00:00 - 23:59
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            DateTime baslangic = DateTime.Today.AddDays(-7);
+            DateTime bitis = DateTime.Now;
+            raporGetir(baslangic, bitis);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            DateTime baslangic = DateTime.Today.AddDays(-30);
+            DateTime bitis = DateTime.Now;
+            raporGetir(baslangic, bitis);
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            ExportToExcel(dataGridView1);
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            VeritabaniYedekAl(kafeOtomasyonu.Database.Db.GetConnection());
         }
     }
 }
